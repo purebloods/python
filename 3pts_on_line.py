39056b7e195c452a0834c021bde18045e8525fcb
purebloods <51003332+purebloods@users.noreply.github.com>
2019-07-02 19:51:24 +0530

    Update 3pts_on_line.py

--- 3pts_on_line.py
+++ 3pts_on_line.py
@@ -1,9 +1,9 @@
-x1= (int) (input("Enter x1:"))
-y1= (int) (input("Enter y1:"))
-x2= (int) (input("Enter x2:"))
-y2= (int) (input("Enter y2:"))
-x3= (int) (input("Enter x3:"))
-y3= (int) (input("Enter y3:"))
+x1= (int) (input())
+y1= (int) (input())
+x2= (int) (input())
+y2= (int) (input())
+x3= (int) (input())
+y3= (int) (input())
 
 a=(((x1-x2)/(y1-y2))-((x3-x1)/(y3-y1)))
 if(a==0):
